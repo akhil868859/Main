854bde1b90cfa395a76c7ab78dcad0559687f202
AKHIL <akhilsuribaba055@gmail.com>
2025-03-22 08:21:04 +0530

    first commit

--- Main.java
+++ Main.java
@@ -1,45 +1,50 @@
 class Employee{
-     private int id;
-     private String name;
-     private float salary;
-     private String department;
+    private int id;
+    private String name;
+    private float salary;
+    private String department;
 
-    public int getId(){
-        return id;
-    }
-    public void setId(int id){
-        this.id = id;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.id = id;
-    }
-    public float getSalary(){
-        return salary;
-    }
-    public void setSalary(float salary){
+   public int getId(){
+       return id;
+   }
+   public void setId(int id){
+       this.id = id;
+   }
+   public String getName(){
+       return name;
+   }
+   public void setName(String name){
+       this.name=name;
+   }
+   public float getSalary(){
+       return salary;
+   }
+   public void setSalary(float salary){
         this.salary =salary;
-    }
-    public String getDepatment(){
-        return department;
-    }
-    public void setDeparment(String department){
-        this.department = department;
-    }
+   }
+   public String getDepartment(){
+       return department;
+   }
+   public void setDepartment(String department){
+       this.department = department;
+   }
 }
 public class Main{
-    public static voide main(String[] args){
+   public static void main(String[] args){
 
-        Employee employee = new Employee();
-        employee.setId(1);
-        employee.setName("JAVA");
-        employee.setSalary(30000.98);
-        employee.setDeparmenu("MCA");
+       Employee employee = new Employee();
+       employee.setId(1);
+       employee.setName("JAVA");
+       employee.setSalary(30000.98f);
+       employee.setDepartment("MCA");
 
-       System.out.println(employee.getId());
-        System.out.println(employee.getName());
+      System.out.println(employee.getId());
+       System.out.println(employee.getName());
+       System.out.println(employee.getSalary());
+       System.out.println(employee.getDepartment());
+       
+   }
+}
         System.out.println(employee.getSalary());
         System.out.println(employee.getDepatment());
         
